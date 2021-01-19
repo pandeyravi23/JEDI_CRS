82638f3a098543cda74712df8800289a90751bf7
Ravi <ravipandeytps@gmail.com>
2021-01-19 20:05:33 +0530

    Added Student class in bean package

--- CRS-JEDI-Flipkart/src/com/flipkart/bean/Student.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/bean/Student.java
@@ -1,5 +1,42 @@
-package com.flipkart.bean;
+package com.flipkart.demo;
+import java.util.ArrayList;
 
-public class Student {
-
+public class Student extends User{
+	
+	private int rollNo;
+	private boolean isRegistered;
+	private ArrayList<Integer> enrolledCourses;
+	public String branch;
+	
+	public void setrollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+	
+	public int getrollNo() {
+		return rollNo;
+	}
+	
+	public boolean getisRegistered() {
+		return isRegistered;
+	}
+	
+	public void setisRegistered(boolean isRegistered) {
+		this.isRegistered = isRegistered;
+	}
+	
+	public ArrayList<Integer> getEnrolledCourses() {
+		return enrolledCourses;
+	}
+	
+	public void setEnrolledCourses(ArrayList<Integer> enrolledCourses) {
+		this.enrolledCourses = enrolledCourses;
+	}
+	
+	public String getBranch() {
+		return branch;
+	}
+	
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
 }
