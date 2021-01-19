aef66636fe377ac84b5cf9824c343ad33d36c613
bhavyagupta98 <bhavyagupta@live.com>
2021-01-19 21:54:45 +0530

    Added loggedin attribute  to user bean

--- CRS-JEDI-Flipkart/src/com/flipkart/bean/User.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/bean/User.java
@@ -5,27 +5,44 @@ public class User {
 	protected String userName;
 	protected String role;
 	protected String email;
+	protected boolean isLoggedIn;
+
+	public boolean isLoggedIn() {
+		return isLoggedIn;
+	}
+
+	public void setLoggedIn(boolean isLoggedIn) {
+		this.isLoggedIn = isLoggedIn;
+	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public int getUserId() {
 		return userId;
 	}
+
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getRole() {
 		return role;
 	}
+
 	public void setRole(String role) {
 		this.role = role;
 	}
