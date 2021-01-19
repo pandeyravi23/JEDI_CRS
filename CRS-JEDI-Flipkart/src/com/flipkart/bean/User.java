b7f20065faad555bf272392c3aa1be3c526e15df
bhavyagupta98 <bhavyagupta@live.com>
2021-01-19 17:31:32 +0530

    Bean file for User updated

--- CRS-JEDI-Flipkart/src/com/flipkart/bean/User.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/bean/User.java
@@ -1,5 +1,25 @@
 package com.flipkart.bean;
 
 public class User {
-
+	protected int userId;
+	protected String userName;
+	protected String role;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
 }
