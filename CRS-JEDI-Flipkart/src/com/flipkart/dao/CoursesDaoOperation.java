40b94b927d7dfb898739f24136dd193fc2b646db
bhavyagupta98 <bhavyagupta@live.com>
2021-01-20 22:11:37 +0530

    Minor functionalities for student added

--- CRS-JEDI-Flipkart/src/com/flipkart/dao/CoursesDaoOperation.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/dao/CoursesDaoOperation.java
@@ -32,10 +32,10 @@ public class CoursesDaoOperation implements CoursesDaoInterface {
 		
 		try {	
 			//inputStream = new FileInputStream("D:/JEDI/testingJava/db/courses.txt");
-//			inputStream = new FileInputStream("/Users/bhavya/Desktop/data.txt");
+			inputStream = new FileInputStream("/Users/bhavya/Desktop/data.txt");
 			//inputStream = new FileInputStream("C:\\Users\\chinm\\OneDrive\\Desktop\\Flipkart_Internship\\JEDI_Bootcamp\\JAVA\\data.txt");
 
-			inputStream = new FileInputStream("C:\\Users\\chinm\\OneDrive\\Desktop\\Flipkart_Internship\\JEDI_Bootcamp\\JEDI_SRS\\CRS-JEDI-Flipkart\\src\\data.txt");
+//			inputStream = new FileInputStream("C:\\Users\\chinm\\OneDrive\\Desktop\\Flipkart_Internship\\JEDI_Bootcamp\\JEDI_SRS\\CRS-JEDI-Flipkart\\src\\data.txt");
 
 			Scanner scanner = new Scanner(inputStream);
 			while(scanner.hasNext()) {
