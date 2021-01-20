116bfa1965d841906e3d7fb397d64eea785f6c3d
Ayush0406 <laddhaayush@gmail.com>
2021-01-20 21:59:19 +0530

    Added relative path functionality

--- CRS-JEDI-Flipkart/src/com/flipkart/dao/CoursesDaoOperation.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/dao/CoursesDaoOperation.java
@@ -34,8 +34,10 @@ public class CoursesDaoOperation implements CoursesDaoInterface {
 			//inputStream = new FileInputStream("D:/JEDI/testingJava/db/courses.txt");
 //			inputStream = new FileInputStream("/Users/bhavya/Desktop/data.txt");
 			//inputStream = new FileInputStream("C:\\Users\\chinm\\OneDrive\\Desktop\\Flipkart_Internship\\JEDI_Bootcamp\\JAVA\\data.txt");
+//			inputStream = new FileInputStream("C:\\Users\\chinm\\OneDrive\\Desktop\\Flipkart_Internship\\JEDI_Bootcamp\\JEDI_SRS\\CRS-JEDI-Flipkart\\src\\data.txt");
 
-			inputStream = new FileInputStream("C:\\Users\\chinm\\OneDrive\\Desktop\\Flipkart_Internship\\JEDI_Bootcamp\\JEDI_SRS\\CRS-JEDI-Flipkart\\src\\data.txt");
+			String filePath = new File("").getAbsolutePath();
+			inputStream = new FileInputStream(filePath.concat("\\src\\data.txt"));
 
 			Scanner scanner = new Scanner(inputStream);
 			while(scanner.hasNext()) {
