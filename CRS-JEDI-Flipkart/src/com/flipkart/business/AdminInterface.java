415ad0683dff9b3b77797f40ebedc7f490f6fe42
Ayush0406 <laddhaayush@gmail.com>
2021-01-20 11:05:17 +0530

    Added the Admin business interface and class files

--- CRS-JEDI-Flipkart/src/com/flipkart/business/AdminInterface.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/business/AdminInterface.java
@@ -2,4 +2,10 @@ package com.flipkart.business;
 
 public interface AdminInterface {
 	
+	public void generateReportCard();
+	public void addProfessor();
+	public void addAdmin();
+	public void approveStudent();
+	public void addCourse();
+	public void deleteCourse();
 }
--- CRS-JEDI-Flipkart/src/com/flipkart/business/AdminOperation.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/business/AdminOperation.java
@@ -1,5 +1,45 @@
 package com.flipkart.business;
 
-public class AdminOperation {
+import org.apache.log4j.Logger;
 
+public class AdminOperation implements AdminInterface{
+
+	public static Logger logger = Logger.getLogger(AdminOperation.class);
+	
+	@Override
+	public void generateReportCard() {
+		// TODO Auto-generated method stub
+		logger.info("In generate report method");
+	}
+
+	@Override
+	public void addProfessor() {
+		// TODO Auto-generated method stub
+		logger.info("In addProfessor method");
+	}
+
+	@Override
+	public void addAdmin() {
+		// TODO Auto-generated method stub
+		logger.info("In addProfesoor method");
+	}
+
+	@Override
+	public void approveStudent() {
+		// TODO Auto-generated method stub
+		logger.info("In addProfesoor method");
+	}
+
+	@Override
+	public void addCourse() {
+		// TODO Auto-generated method stub
+		logger.info("In addProfesoor method");
+	}
+
+	@Override
+	public void deleteCourse() {
+		// TODO Auto-generated method stub
+		logger.info("In addProfesoor method");
+	}
+	
 }
