1af01a7372be099a3a562b61024027f00b05e8da
Ravi <ravipandeytps@gmail.com>
2021-01-20 12:52:03 +0530

    Added ReportCard Interface

--- CRS-JEDI-Flipkart/src/com/flipkart/business/ReportCardInterface.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/business/ReportCardInterface.java
@@ -1,5 +1,10 @@
 package com.flipkart.business;
+import com.flipkart.bean.Student;
+import com.flipkart.bean.Admin;
 
 public interface ReportCardInterface {
 
+	void printGradeReport(int adminId, Student student);
+
+	void emailGradeReport(int adminId, Student student);
 }
--- CRS-JEDI-Flipkart/src/com/flipkart/business/ReportCardOperation.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/business/ReportCardOperation.java
@@ -1,5 +1,15 @@
 package com.flipkart.business;
+import org.apache.log4j.Logger;
+import com.flipkart.bean.Student;
+import com.flipkart.bean.Admin;
 
 public class ReportCardOperation {
 
+	void printGradeReport(int adminId, Student student){
+		logger.info("Inside printGradeReport Method");
+	}
+
+	void emailGradeReport(int adminId, Student student){
+		logger.info("Inside emailGradeReport Method");
+	}
 }
