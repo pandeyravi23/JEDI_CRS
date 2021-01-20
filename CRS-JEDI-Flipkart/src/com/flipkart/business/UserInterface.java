a0731902f9ff4237db1acda457fa90367f177686
Partha <parthapaul47@gmail.com>
2021-01-20 14:10:02 +0530

    Fixed errors

--- CRS-JEDI-Flipkart/src/com/flipkart/business/UserInterface.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/business/UserInterface.java
@@ -1,15 +1,5 @@
 package com.flipkart.business;
 
-import org.apache.log4j.Logger;
-
 public interface UserInterface {
-	default public void viewCatalog() {
-
-		Logger logger = Logger.getLogger(UserInterface.class);
-
-		CourseCatalogOperation catalogOperation = new CourseCatalogOperation();
-
-		logger.info("Course Id\tCourse Name\tFees\tCourse Description");
-
-	}
+	
 }
--- CRS-JEDI-Flipkart/src/com/flipkart/client/UserClient.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/client/UserClient.java
@@ -1,5 +1,16 @@
 package com.flipkart.client;
 
-public class UserClient {
+import org.apache.log4j.Logger;
 
+public class UserClient {
+	private static Logger logger = Logger.getLogger(UserClient.class);
+	
+	public static void main(String[] args) {
+		logger.info("Welcome!!!");
+		showMenu();
+	}
+	
+	private static void showMenu() {
+		System.out.println("Menu");
+	}
 }
