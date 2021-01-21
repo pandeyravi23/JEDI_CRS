41e56ef541f6a435c4eac0e81b964df383e86536
bhavyagupta98 <bhavyagupta@live.com>
2021-01-21 22:56:49 +0530

    ProfessorDao modified

--- CRS-JEDI-Flipkart/src/com/flipkart/util/DBConnection.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/util/DBConnection.java
@@ -20,6 +20,7 @@ public class DBConnection {
 	static final String DB_URL = "jdbc:mysql://localhost/test";
 	static final String USER = "root";
 	static final String PASS = "99Partha19op@#";
+//	static final String PASS = "bernabeu";
 
 	public static Connection getConnection() {
 		if (connection != null) {
