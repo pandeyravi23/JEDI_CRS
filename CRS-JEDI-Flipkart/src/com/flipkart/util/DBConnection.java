c8fe48b891b7e6b01b622d9f696858ec78b81a6a
Chinmay Sharma <chinmaysharmacs10@gmail.com>
2021-01-22 00:12:41 +0530

    Student conflict resolved

--- CRS-JEDI-Flipkart/src/com/flipkart/util/DBConnection.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/util/DBConnection.java
@@ -19,7 +19,7 @@ public class DBConnection {
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	static final String DB_URL = "jdbc:mysql://localhost/test";
 	static final String USER = "root";
-	static final String PASS = "99Partha19op@#";
+	static final String PASS = "invictus@1999";
 
 	public static Connection getConnection() {
 		if (connection != null) {
