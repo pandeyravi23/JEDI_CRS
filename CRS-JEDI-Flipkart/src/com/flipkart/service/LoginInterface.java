aa58689e44ecaa4876a1087f4608bceb96663aee
bhavyagupta98 <bhavyagupta@live.com>
2021-01-21 18:44:18 +0530

    Changes in login functionality

--- CRS-JEDI-Flipkart/src/com/flipkart/service/LoginInterface.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/service/LoginInterface.java
@@ -5,11 +5,12 @@ import com.flipkart.bean.Professor;
 import com.flipkart.bean.Student;
 
 public interface LoginInterface {
-	public boolean login(String username, String password);
+	public int login(String username, String password);
 
 	public void registerStudent(Student student, String password);
 
 	public void registerProfessor(Professor professor, String password);
 
 	public void registerAdmin(Admin admin, String password);
+
 }
