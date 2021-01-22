6e81d21cc479af41e77ed435d3aa448a1e9cfeca
bhavyagupta98 <bhavyagupta@live.com>
2021-01-22 13:56:32 +0530

    Update grade fucbtionality addaed

--- CRS-JEDI-Flipkart/src/com/flipkart/client/ProfessorMenu.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/client/ProfessorMenu.java
@@ -10,62 +10,77 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ProfessorMenu {
-    private static Logger logger = Logger.getLogger(ProfessorMenu.class);
-    private ProfessorInterface professorOperation = new ProfessorOperation();
+	private static Logger logger = Logger.getLogger(ProfessorMenu.class);
+	private ProfessorInterface professorOperation = new ProfessorOperation();
 
-    public void professorClient(Professor professor){
-    	logger.info("Welcome Professor " + professor.getUserName() + "!!");
-        int choice;
-        Scanner input = new Scanner(System.in);
-        do{
-            showChoices();
-            choice = input.nextInt();
+	public void professorClient(Professor professor) {
+		logger.info("Welcome Professor " + professor.getUserName() + "!!");
+		int choice;
+		Scanner input = new Scanner(System.in);
+		do {
+			showChoices();
+			choice = input.nextInt();
 
-            switch (choice){
-                case -1:
-                    logger.info(".....Logging Out.....\n");
-                    break;
-                case 1:
-                    gradeStudents();
-                    break;
-                case 2:
-                    allotedCourse(professor.getUserId());
-                    break;
-                case 3:
-                    viewStudents();
-                    break;
-                default:
-                    logger.info("Invalid choice\n");
-                    break;
-            }
-        }while (choice!=-1);
-    }
-    
-    
-    
-    public void init(String email) {
-    	Professor professor = professorOperation.getProfessorByEmail(email);
-    	professorClient(professor);
-    }
+			switch (choice) {
+			case -1:
+				logger.info(".....Logging Out.....\n");
+				break;
+			case 1:
+				updateStudentGrades();
+				break;
+			case 2:
+				allotedCourse(professor.getUserId());
+				break;
+			case 3:
+				viewStudents();
+				break;
+			default:
+				logger.info("Invalid choice\n");
+				break;
+			}
+		} while (choice != -1);
+	}
 
-    public static void showChoices(){
-        logger.info("Please select an operation: ");
-        logger.info("1. Grade Students");
-        logger.info("2. View Alloted Courses");
-        logger.info("3. View Students Enrolled in a course ");
-        logger.info("-1 to Logout");
-    }
+	public void init(String email) {
+		Professor professor = professorOperation.getProfessorByEmail(email);
+		professorClient(professor);
+	}
 
-    // method to add course
-    public void gradeStudents(){
-        logger.info("Inside Grade Student Method");
-    }
+	public static void showChoices() {
+		logger.info("Please select an operation: ");
+		logger.info("1. Grade Students");
+		logger.info("2. View Alloted Courses");
+		logger.info("3. View Students Enrolled in a course ");
+		logger.info("-1 to Logout");
+	}
 
-    // method to drop a course
-    public void allotedCourse(int professorId){
-    	ProfessorOperation professorOperation = new ProfessorOperation();
-    	professorOperation.showCourses(professorId);
-    }
+	// method to add course
+	public void updateStudentGrades() {
+		logger.info("Inside Grade Student Method");
+		Scanner sc = new Scanner(System.in);
+		try {
+
+			logger.info("Enter courseId");
+			int courseId = sc.nextInt();
+			logger.info("Enter studentId");
+			int studentId = sc.nextInt();
+			logger.info("Enter Grade");
+			String grade = sc.next();
+
+			ProfessorOperation professorOperation = new ProfessorOperation();
+			boolean status = professorOperation.updateStudentGrade(courseId, studentId, grade);
+			logger.info("Update Grade Status : " + status + "\n\n");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+
+	// method to drop a course
+	public void allotedCourse(int professorId) {
+		ProfessorOperation professorOperation = new ProfessorOperation();
+		professorOperation.showCourses(professorId);
+	}
 
 	// method to make payment
 	public void viewStudents() {
@@ -78,10 +93,6 @@ public class ProfessorMenu {
 			professorOperation.viewStudentsEnrolled(courseId);
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			if (sc != null) {
-				sc.close();
-			}
-		}
+		} 
 	}
 }
