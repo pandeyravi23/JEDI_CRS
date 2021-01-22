d95b876fbb4ec220a9ad37984ba03487c4d7be6e
Ayush0406 <laddhaayush@gmail.com>
2021-01-22 14:38:58 +0530

    Added the addProf and verifyEmail functionality to Admin

--- CRS-JEDI-Flipkart/src/com/flipkart/dao/AdminDAOOperation.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/dao/AdminDAOOperation.java
@@ -2,36 +2,96 @@ package com.flipkart.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import org.apache.log4j.Logger;
 
+import com.flipkart.bean.Professor;
 import com.flipkart.util.DBConnection;
 
 public class AdminDAOOperation {
 	
 	private static Logger logger = Logger.getLogger(AdminDAOOperation.class);
-	Connection connection = null;
+	Connection connection = DBConnection.getConnection();
 	PreparedStatement ps = null;
 	
-	public boolean addAdmin(String userName, String password)
+//	returns false if the email is already present in the database. Returns true otherwise.
+	public boolean verifyEmail(String email)
 	{
 		try
 		{
-			connection = DBConnection.getConnection();
+			String sqlQuery = "select email from credentials where email = ?";
+			ps = connection.prepareStatement(sqlQuery);
+			
+			ps.setString(1, email);
+			ResultSet rs =  ps.executeQuery();
+			
+			if(rs.next())
+				return false;
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}	
+		return true;
+	}
+	
+	public int addAdmin(String email, String password)
+	{
+		try
+		{
+			if(verifyEmail(email) == false)
+			{
+				return 2;
+			}
 			String sqlQuery = "insert into credentials(role, email, password) values(?, ?, ?)";
 			ps = connection.prepareStatement(sqlQuery);
 			
 			ps.setInt(1, 3);
-			ps.setString(2,  userName);
+			ps.setString(2,  email);
 			ps.setString(3, password);
 			
 			ps.executeUpdate();
-			return true;
+			return 1;
 		}catch(Exception e)
 		{
 			e.printStackTrace();
 		}
-		return false;
+		return 0;
+	}
+	
+	public int addProfessor(String password, Professor prof)
+	{
+		try
+		{
+			String credQuery = "insert into credentials(role, email, password) values (?, ?, ?)";
+			ps = connection.prepareStatement(credQuery, Statement.RETURN_GENERATED_KEYS);
+			ps.setInt(1,  2);
+			ps.setString(2,  prof.getEmail());
+			ps.setString(3, password);
+			
+			ps.executeUpdate();
+			ResultSet rs = ps.getGeneratedKeys();
+			
+			rs.next();
+			prof.setUserId(rs.getInt(1));
+			
+			String profQuery = "insert into professor values(?, ?, ?, ?, ?)";
+			ps = connection.prepareStatement(profQuery);
+			ps.setInt(1, prof.getUserId());
+			ps.setString(2, prof.getUserName());
+			ps.setString(3, prof.getEmail());
+			ps.setString(4, prof.getRole());
+			ps.setString(5,  prof.getDepartment());
+			ps.executeUpdate();
+			
+			return 1;
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return 0;
 	}
 	
 }
--- CRS-JEDI-Flipkart/src/com/flipkart/service/AdminOperation.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/service/AdminOperation.java
@@ -1,7 +1,10 @@
 package com.flipkart.service;
 
+import java.util.Scanner;
+
 import org.apache.log4j.Logger;
 
+import com.flipkart.bean.Professor;
 import com.flipkart.dao.AdminDAOOperation;
 
 public class AdminOperation implements AdminInterface{
@@ -18,15 +21,65 @@ public class AdminOperation implements AdminInterface{
 	@Override
 	public void addProfessor() {
 		// TODO Auto-generated method stub
-		logger.info("In addProfessor method");
+		Scanner sc = new Scanner(System.in);
+		
+		logger.info("Enter the new email : ");
+		
+		String email = sc.nextLine();
+		
+		String pwd1 = "abc";
+		String pwd2 = "xyz";
+		
+		while(!pwd1.equals(pwd2)) {
+			logger.info("Enter password : ");
+			pwd1 = sc.nextLine();
+			logger.info("Re-enter password : ");
+			pwd2 = sc.nextLine();
+			if(!pwd1.equals(pwd2))
+			{
+				logger.info("Passwords do not match. Please re-enter.");
+			}
+		}
+		
+		if(!adminDAO.verifyEmail(email))
+		{
+			logger.info("Email already exists.Please retry.");
+			sc.close();
+			return;
+		}
+		
+		logger.info("Please enter your name : ");
+		Professor prof = new Professor();
+		prof.setUserName(sc.nextLine());
+		logger.info("Enter your designation : ");
+		prof.setRole(sc.nextLine());
+		logger.info("Enter your department : ");
+		prof.setDepartment(sc.nextLine());
+		prof.setEmail(email);
+		
+		int res = adminDAO.addProfessor(pwd1, prof);
+		
+		if(res == 1)
+		{
+			logger.info("Professor successfully added.");
+		}
+		else
+		{
+			logger.info("Unable to add professor.");
+		}
 	}
 
 	@Override
 	public void addAdmin(String username, String password) {
 		// TODO Auto-generated method stub
-		if(adminDAO.addAdmin(username, password))
+		int res = adminDAO.addAdmin(username, password);
+		if(res == 1)
 		{
 			logger.info("Admin added successfully");
+		}
+		else if(res == 2)
+		{
+			logger.info("Email already exists. Please retry.");
 		}
 		else
 		{
