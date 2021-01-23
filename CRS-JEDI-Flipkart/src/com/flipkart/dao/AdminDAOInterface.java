6aa523f9c51c884e38f23fa9ba1c17f81e7e5bfa
Ravi <ravipandeytps@gmail.com>
2021-01-24 01:03:33 +0530

    Conflicts Removed

--- CRS-JEDI-Flipkart/src/com/flipkart/dao/AdminDAOInterface.java
+++ CRS-JEDI-Flipkart/src/com/flipkart/dao/AdminDAOInterface.java
@@ -5,6 +5,7 @@ package com.flipkart.dao;
 
 import com.flipkart.bean.Admin;
 import com.flipkart.bean.Professor;
+import com.flipkart.exception.AdminCRSException;
 
 /**
  * @author jedi04
@@ -12,7 +13,7 @@ import com.flipkart.bean.Professor;
  */
 public interface AdminDAOInterface {
 	public void printGrades(int studentId);
-	public boolean verifyEmail(String email);
+	public boolean verifyEmail(String email) throws AdminCRSException;
 	public int addAdmin(String password, Admin admin);
 	public int addProfessor(String password, Professor prof);
 	public void allotCourses(int courseId,int professorId);
