13c75ea93c37fc013aa7402ee62c63749e4f0197
Ayush0406 <laddhaayush@gmail.com>
2021-01-29 16:54:44 +0530

    Added getReportCard function in admin

--- CRS-JEDI-Flipkart-REST/src/com/flipkart/RESTController/AdminRESTAPI.java
+++ CRS-JEDI-Flipkart-REST/src/com/flipkart/RESTController/AdminRESTAPI.java
@@ -3,10 +3,30 @@
  */
 package com.flipkart.RESTController;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.flipkart.dao.AdminDAOOperation;
+
 /**
  * @author JEDI04
  *
  */
-public class AdminRESTAPI {
 
+@Path("/admin")
+public class AdminRESTAPI {
+	
+	
+	@GET
+	@Path("/getReportCard")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void getReportCard(@QueryParam("id") Integer id)
+	{
+		AdminDAOOperation adminDAO = AdminDAOOperation.getInstance();
+		adminDAO.printGrades(id);
+		
+	}
 }
